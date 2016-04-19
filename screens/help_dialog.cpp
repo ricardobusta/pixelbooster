0669f22614d717599af8282b4b64089797b5ae46
Ricardo Bustamante <ricardobqueiroz@gmail.com>
2016-04-19 15:42:44 -0300

    Adding images to the help file! :)
    Now all We have to do is to finish it's content!

--- screens/help_dialog.cpp
+++ screens/help_dialog.cpp
@@ -5,6 +5,10 @@
 #include <QTextStream>
 #include <QDebug>
 
+// Replaces image url on html to the Qt internal URL format.
+const QRegularExpression kUrlRegex("src\\s*=\\s*(\\'|\\\")");
+const QString kUrlReplacement("src=\\1:/");
+
 HelpDialog::HelpDialog(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::HelpDialog)
@@ -24,11 +28,14 @@ void HelpDialog::OpenPage(const QString &file_name)
     QFile file(":/"+file_name);
     file.open(QFile::ReadOnly|QFile::Text);
     QTextStream stream(&file);
-    ui->textBrowser->setHtml(stream.readAll());
+    QString html = stream.readAll();
+    html = html.replace(kUrlRegex,kUrlReplacement);
+    ui->textBrowser->setHtml(html);
     connect(ui->textBrowser,SIGNAL(anchorClicked(QUrl)),this,SLOT(HandleAnchor(QUrl)));
 }
 
 void HelpDialog::HandleAnchor(const QUrl &url)
 {
+    qDebug() << "opening" << url.fileName();
     OpenPage(url.fileName());
 }
