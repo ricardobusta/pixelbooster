dc35396c3167ccf49cbd5910deee2d31a42fa713
Ricardo Bustamante <ricardobqueiroz@gmail.com>
2017-03-14 01:31:44 -0300

    Starting out a refactor from scratch.

--- View/main.cpp
+++ View/main.cpp
@@ -1,27 +1,11 @@
-/***************************************************************************\
-*  Pixel::Booster, a simple pixel art image editor.                         *
-*  Copyright (C) 2015  Ricardo Bustamante de Queiroz (ricardo@busta.com.br) *
-*  Visit the Official Homepage: pixel.busta.com.br                          *
-*                                                                           *
-*  This program is free software: you can redistribute it and/or modify     *
-*  it under the terms of the GNU General Public License as published by     *
-*  the Free Software Foundation, either version 3 of the License, or        *
-*  (at your option) any later version.                                      *
-*                                                                           *
-*  This program is distributed in the hope that it will be useful,          *
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of           *
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            *
-*  GNU General Public License for more details.                             *
-*                                                                           *
-*  You should have received a copy of the GNU General Public License        *
-*  along with this program.  If not, see <http://www.gnu.org/licenses/>.    *
-\***************************************************************************/
+#include "mainwindow.h"
+#include <QApplication>
 
-#include "application/pixel_booster.h"
+int main(int argc, char *argv[])
+{
+  QApplication a(argc, argv);
+  MainWindow w;
+  w.show();
 
-#include "utils/debug.h"
-
-int main(int argc, char **argv) {
-  PixelBooster a(argc, argv);
   return a.exec();
 }
